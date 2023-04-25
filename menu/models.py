d0f62d04fdfe0be1e78ca202bd9c0dc8010c9c2c
a-rod85 <31aaronanderson@gmail.com>
2023-04-25 14:02:13 +0000

    add to models.py in menu app

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class MenuSection(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Food(models.Model):
+    menu_section = models.ForeignKey(MenuSection, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Drink(models.Model):
+    menu_section = models.ForeignKey(MenuSection, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name

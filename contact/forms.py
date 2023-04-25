134fc0a4fa4d79538a22eec87796a50b3bdb6595
a-rod85 <31aaronanderson@gmail.com>
2023-04-25 10:27:05 +0000

    add forms.py to contact

---
+++ contact/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+
+
+class ContactForm(forms.Form):
+
+    first_name = forms.CharField(max_length=60)
+    last_name = forms.CharField(max_length=60)
+    email_address = forms.EmailField(max_length=100)
+    message = forms.CharField(widget=forms.Textarea, max_length=1500)
